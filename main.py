5aa37c6d4bca25a0555fef788ab33e96135c629d
Buda <duwerts@gmail.com>
2023-09-12 22:38:16 +0300

    Зробив інші зміни

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-
+import re
 def parse(query: str) -> dict:
+    pattern = r'\?([^#]*)'
+    match = re.search(pattern, query)
+    print(match)
 
-    return {}
+    if match:
+        query_string = match.group(1)
+        params = re.findall(r'([^&]+)=([^&]+)', query_string)
+        print(params)
+        return {param[0]: param[1] for param in params}
+
+    else:
+        return {}
 
 
 if __name__ == '__main__':
@@ -9,7 +19,7 @@ if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://example.com/?name=Dima')
 
 
 def parse_cookie(query: str) -> dict:
